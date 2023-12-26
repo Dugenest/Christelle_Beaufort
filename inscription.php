f373210dd506bc85d6b8ebde7753a443ee6871e6
Dugenest <sebastien.dugenest@gmail.com>
2023-12-26 19:32:57 +0100

    ajout php formulaire

--- inscription.php
+++ inscription.php
@@ -1,3 +1,104 @@
+<?php
+
+//création d'une constante pour une régex du nom
+define('NAME', '^[a-zA-Z0-9]{2,30}$');
+
+//Condition principale pour tous les input (es ce que la méthode de récupération est bien 'POST'?)
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    //Création d'un tableau d'erreurs
+        $error = [];
+    //Récupération et nettoyage de la récupération de la donnée "lastName"
+        $lastName = filter_input(INPUT_POST, 'lastName', FILTER_SANITIZE_SPECIAL_CHARS);
+        if (empty($lastName)) {
+            $error['lastName'] = 'Le Nom est obligatoire';
+        } else {
+    //Validation de la donnée "lastName" grâce à la regex
+            $isOk = filter_var($lastName, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>'/^[a-zA-Z0-9]{2,30}$/')));
+            if ($isOk == false){
+                $error['lastName'] = 'Le Nom n\'est pas valide !';
+            }
+        }
+    
+    //Récupération et nettoyage de la récupération de la donnée "firstName"
+    $firstName = filter_input(INPUT_POST, 'firstName', FILTER_SANITIZE_SPECIAL_CHARS);
+    if (empty($firstName)) {
+        $error['firstName'] = 'Le Prénom est obligatoire';
+    } else {
+    //Validation de la donnée "firstName" grâce à la regex
+        $isOk = filter_var($firstName, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>'/^[a-zA-Z0-9]{2,30}$/')));
+        if ($isOk == false){
+            $error['firstName'] = 'Le Prénom n\'est pas valide !';
+        }
+    }
+
+    //Récupération, nettoyage et validation de la donnée "Email"
+        $Email = filter_input(INPUT_POST, 'Email', FILTER_SANITIZE_EMAIL);
+        if (empty($Email)) {
+            $error['Email'] = 'L\'Email est obligatoire';
+        } else {
+            $isOk = filter_var($Email, FILTER_VALIDATE_EMAIL);
+            if ($isOk == false){
+                $error['Email'] = 'l\'Email n\'est pas valide !';
+            }
+        }
+    
+    //Récupération et nettoyage de la récupération de la donnée "Phone"
+        $Phone = filter_input(INPUT_POST, 'Phone', FILTER_SANITIZE_NUMBER_INT);
+        if (empty($Phone)) {
+    //Validation de la donnée "Phone" grâce à la regex
+        $isOk = filter_var($Phone, FILTER_VALIDATE_INT);
+        if ($isOk == false){
+            $error['Phone'] = 'Le numéro de téléphone n\'est pas valide !';
+        }
+    }
+
+    //Récupération, nettoyage et validation de la donnée "Adress"
+        $Adress = filter_input(INPUT_POST, 'Adress', FILTER_SANITIZE_SPECIAL_CHARS);
+        if (!empty($Adress)) {
+    //Validation de la donnée "Adress" grâce à la regex
+        $isOk = filter_var($Adress, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>'/^[A-Za-z0-9À-ÖØ-öø-ÿéè\s\.,;\'\"!?()\[\]{}\-:]{1,300}$/')));
+        if ($isOk == false){
+            $error['Adress'] = 'L\'Adresse décrite n\'est pas valide !';
+        }
+    }
+
+    //Récupération et nettoyage de la récupération de la donnée "userName"
+        $userName = filter_input(INPUT_POST, 'userName', FILTER_SANITIZE_SPECIAL_CHARS);
+        if (empty($userName)) {
+            $error['userName'] = 'L\'identifiant est obligatoire';
+        } else {
+    //Validation de la donnée "userName" grâce à la regex
+        $isOk = filter_var($userName, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>'/^[a-zA-Z0-9]{2,30}$/')));
+        if ($isOk == false){
+            $error['userName'] = 'L\'identifiant n\'est pas valide !';
+        }
+    }
+
+    //Récupération, nettoyage et validation de la donnée "Mot de passe"
+        $Password1 = filter_input(INPUT_POST, 'Password1', FILTER_SANITIZE_SPECIAL_CHARS);
+        if (empty($Password1)) {
+            $error['Password1'] = 'Le Mot de Passe est obligatoire';
+        } else {
+            $isOk = filter_var($Password1, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>'/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/')));
+            if ($isOk == false){
+                $error['Password1'] = 'Le Mot de Passe n\'est pas valide !';
+            }
+        }
+    
+    //Récupération, nettoyage et validation de la donnée "Mot de passe"
+        $Password2 = filter_input(INPUT_POST, 'Password2', FILTER_SANITIZE_SPECIAL_CHARS);
+        if (empty($Password2)) {
+            $error['Password2'] = 'Le Mot de Passe est obligatoire';
+        } else {
+            $isOk = filter_var($Password2, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>'/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/')));
+            if ($isOk == false){
+                $error['Password2'] = 'Le Mot de Passe n\'est pas valide !';
+            }
+        }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -16,57 +117,103 @@
     <main>
         <div class="container-fluid py-5">
             <h1>INSCRIPTION</h1>
-            <form class="row d-flex justify-content-center" id="formulaire">
-                <div class="col-md-6">
-                    <div class="col-12  mb-3 py-3">
-                        <label for="lastname" class="form-label">Nom<strong>*</strong></label>
-                        <input type="text" class="form-control" id="lastname" aria-describedby="nom" placeholder="Votre nom" required>
-                    </div>
-                    <div class="col-12  mb-3 py-3">
-                        <label for="firstname" class="form-label">Prénom<strong>*</strong></label>
-                        <input type="text" class="form-control" id="firstname" aria-describedby="prenom" placeholder="Votre prénom" required>
-                    </div>
-                    <div class="col-12  mb-3 py-3">
-                        <label for="email" class="form-label">Email<strong>*</strong></label>
-                        <input type="email" class="form-control" id="email" aria-describedby="email" placeholder="Votre email" required>
-                    </div>
-                    <div class="col-12  mb-3 py-3">
-                        <label for="phone" class="form-label">Téléphone</label>
-                        <input type="tel" class="form-control" id="phone" aria-describedby="telephone" placeholder="Votre numéro de téléphone">
-                    </div>
-                    <div class="col-12  mb-3 py-3">
-                        <label for="adress" class="form-label">Adresse</label>
-                        <textarea id="adress" class="form-control" rows="3" placeholder="Votre adresse"></textarea>
-                    </div>
-                    <div class="col-12  mb-3 py-3">
-                        <label for="identifiant" class="form-label">Identifiant<strong>*</strong></label>
-                        <input type="text" class="form-control" id="identifiant" aria-describedby="identifiant" placeholder="Votre identifiant" required>
-                    </div>
-                    <div class="col-12  mb-3 py-3">
-                        <label for="Password" class="form-label">Mot de passe<strong>*</strong></label>
-                        <input type="password" class="form-control" id="Password" aria-describedby="password" placeholder="Votre mot de passe" required>
-                    </div>
-                    <div class="col-12  mb-3 py-3">
-                        <label for="Password2" class="form-label">Confirmation mot de passe<strong>*</strong></label>
-                        <input type="password2" class="form-control" id="Password2" aria-describedby="password" placeholder="Confirmer votre mot de passe" required>
-                    </div>
-                    <div class="mb-3">
-                        <div class="form-check">
-                            <label class="form-check-label" for="disabledFieldsetCheck">
-                            <input class="form-check-input" type="checkbox" id="disabledFieldsetCheck" required>
-                                J’accepte les conditions générales du site et J’accepte que mes information soient 
-                                utilisées uniquement dans le cadre de ma demande et de la relation commerciale 
-                                éthique et personnalisée qui peut en découler
-                            </label>
+            <?php if ($_SERVER['REQUEST_METHOD'] != 'POST' || !empty($error)) { ?>
+                <form class="row d-flex justify-content-center" id="formulaire" method="post">
+                    <div class="col-md-6">
+                        <div class="col-12  mb-3 py-3">
+                            <label for="lastName" class="form-label">Nom<strong>*</strong></label>
+                            <input type="text" class="form-control" id="lastName" aria-describedby="nom" name="lastName" value="<?=$lastName??''?>" maxlength='30' autocomplete="lastName" placeholder="Votre nom" required>
+                            <div class="error">
+                                <p id="error1" class="d-none"></p><br>
+                                <!-- coalescente du message d'erreur de lastName -->
+                                <?= $error['lastName'] ?? ''?><br>
+                            </div>
+                        </div>
+                        <div class="col-12  mb-3 py-3">
+                            <label for="firstName" class="form-label">Prénom<strong>*</strong></label>
+                            <input type="text" class="form-control" id="firstName" aria-describedby="prénom" name="firstName" value="<?=$firstName??''?>" maxlength='30' autocomplete="firstName" placeholder="Votre prénom" required>
+                            <div class="error">
+                                <p id="error2" class="d-none"></p><br>
+                                <?= $error['firstName'] ?? ''?><br>
+                            </div>
+                        </div>
+                        <div class="col-12  mb-3 py-3">
+                            <label for="Email" class="form-label">Email<strong>*</strong></label>
+                            <input type="email" class="form-control" id="Email" aria-describedby="email" name="Email" value="<?=$Email??''?>" autocomplete="Email" placeholder="Votre email" required>
+                            <div class="error">
+                                <p id="error3" class="d-none"></p><br>
+                                <?= $error['Email'] ?? ''?><br>
+                            </div>
+                        </div>
+                        <div class="col-12  mb-3 py-3">
+                            <label for="Phone" class="form-label">Téléphone</label>
+                            <input type="tel" class="form-control" id="Phone" aria-describedby="téléphone" name="Phone" value="<?=$Phone??''?>" autocomplete="Phone" placeholder="Votre numéro de téléphone">
+                            <div class="error">
+                                <p id="error4" class="d-none"></p><br>
+                                <?= $error['Phone'] ?? ''?><br>
+                            </div>
+                        </div>
+                        <div class="col-12  mb-3 py-3">
+                            <label for="Adress" class="form-label">Adresse</label>
+                            <textarea id="Adress" class="form-control" rows="3" name="Adress" value="<?=$Adress??''?>" autocomplete="Adress" placeholder="Votre adresse"></textarea>
+                            <div class="error">
+                                <p id="error5" class="d-none"></p><br>
+                                <?= $error['Adress'] ?? ''?><br>
+                            </div>
+                        </div>
+                        <div class="col-12  mb-3 py-3">
+                            <label for="userName" class="form-label">Identifiant<strong>*</strong></label>
+                            <input type="text" class="form-control" id="userName" aria-describedby="identifiant" name="userName" value="<?=$userName??''?>" maxlength='30' autocomplete="userName" placeholder="Votre identifiant" required>
+                            <div class="error">
+                                <p id="error6" class="d-none"></p><br>
+                                <?= $error['userName'] ?? ''?><br>
+                            </div>
+                        </div>
+                        <div class="col-12  mb-3 py-3">
+                            <label for="Password1" class="form-label">Mot de passe<strong>*</strong></label>
+                            <input type="password" class="form-control" id="Password1" aria-describedby="password" name="Password1" value="<?=$Password1??''?>" placeholder="Votre mot de passe" required>
+                            <div class="error">
+                                <p id="error7" class="d-none"></p><br>
+                                <?= $error['Password1'] ?? ''?><br>
+                            </div>                   
+                        </div>
+                        <div class="col-12  mb-3 py-3">
+                            <label for="Password2" class="form-label">Confirmation mot de passe<strong>*</strong></label>
+                            <input type="password" class="form-control" id="Password2" aria-describedby="password" name="Password2" value="<?=$Password2??''?>" placeholder="Confirmer votre mot de passe" required>
+                            <div class="error">
+                                <p id="error8" class="d-none"></p><br>
+                                <p id="error9" class="d-none"></p><br>
+                                <?= $error['Password2'] ?? ''?><br>
+                            </div>
+                        <div class="mb-3">
+                            <div class="form-check">
+                                <label class="form-check-label" for="disabledFieldsetCheck">
+                                <input class="form-check-input" type="checkbox" id="disabledFieldsetCheck" required>
+                                    J’accepte les conditions générales du site et J’accepte que mes information soient 
+                                    utilisées uniquement dans le cadre de ma demande et de la relation commerciale 
+                                    éthique et personnalisée qui peut en découler
+                                </label>
+                            </div>
+                        </div>
+                        <div class="row text-center justify-content-center py-3">
+                            <button type="submit" id="btnInscription" class="btn btn-light">S'inscrire</button>
                         </div>
                     </div>
-                    <div class="row text-center justify-content-center py-3">
-                        <button type="submit" id="btnInscription" class="btn btn-light">S'inscrire</button>
-                    </div>
+                </form>
+                <?php } else { ?>
+                <div class="Data"><br><br>
+                    <h2>Récapitulatif des données du formulaire</h2>
+                        <p>Nom : <?=$lastName?></p>
+                        <p>Prénom : <?=$firstName?></p> 
+                        <p>Email : <?=$Email?></p> 
+                        <p>Téléphone : <?=$Phone?></p> 
+                        <p>Adresse : <?=$Adress?></p> 
+                        <p>Identifiant : <?=$userName?></p>
+                    <?php } ?>
                 </div>
-            </form>
         </div>
     </main>
 <!-- fin du main -->
 
+    <script src="./public/assets/js/scriptInscription.js"></script>
     <?php require_once(__DIR__ . '/footer.php'); ?>
---
+++ public/assets/js/scriptInscription.js
@@ -0,0 +1,143 @@
+// Déclaration des variables
+const lastName = document.getElementById("lastName");
+const firstName = document.getElementById("firstName"); 
+const email = document.getElementById("Email"); 
+const phone = document.getElementById("Phone");
+const adress = document.getElementById("Adress");
+const userName = document.getElementById("userName");
+const pwdMedium = document.getElementById("Password1");
+const pwdStrong = document.getElementById("Password2");
+
+
+const message1 = "Caractères non pris en compte !";
+const message2 = "E-mail incorrect !";
+const message3 = "Mot de Passe différent du premier !";
+const message4 = "Merci de mettre au moins 1 Majuscule, 1 minuscule, 1 chiffre et 1 caratère spécial (min 8)."
+
+
+const errorMessage1 = document.getElementById("error1");
+const errorMessage2 = document.getElementById("error2");
+const errorMessage3 = document.getElementById("error3");
+const errorMessage4 = document.getElementById("error4");
+const errorMessage5 = document.getElementById("error5");
+const errorMessage6 = document.getElementById("error6");
+const errorMessage7 = document.getElementById("error7");
+const errorMessage8 = document.getElementById("error8");
+const errorMessage9 = document.getElementById("error9");
+
+
+// Création des regex
+const regexName = /^[a-zA-Z0-9]{2,30}$/; 
+const regexEmail = /^[A-Za-z0-9.\_\.\-]+@[a-z0-9\_\-]+\.[a-z]{2,5}$/; 
+const regexPhone = /^[0-9]{10}$/;
+const regexAdress = /^[A-Za-z0-9À-ÖØ-öø-ÿéè\s\.,;\'\"!?()\[\]{}\-:]{1,300}$/;
+const regexPwdStrong = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+
+// Création d'un écouteur d'événement pour le lastname
+lastName.addEventListener("keyup", function () { 
+// Supprimer les classes "red" et "green" à chaque saisie 
+    lastName.classList.remove("red", "green"); 
+    errorMessage1.classList.add("d-none");
+// Utilisation de regexName.test() pour tester la valeur 
+// par défaut ici c'est === True
+    if (regexName.test(lastName.value)) { 
+        lastName.classList.add("green"); 
+        errorMessage1.classList.add("d-none");
+    } else { 
+        lastName.classList.add("red"); 
+        errorMessage1.classList.remove("d-none");
+        errorMessage1.textContent = message1; }; 
+});
+
+// Création d'un écouteur d'événement pour le firstname
+firstName.addEventListener("keyup", function () { 
+    // Supprimer les classes "red" et "green" à chaque saisie 
+        firstName.classList.remove("red", "green"); 
+        errorMessage2.classList.add("d-none");
+    //Test de la regexName pour tester la valeur
+        if (regexName.test(firstName.value)) { 
+            firstName.classList.add("green"); 
+            errorMessage2.classList.add("d-none");
+        } else { 
+            firstName.classList.add("red"); 
+            errorMessage2.classList.remove("d-none");
+            errorMessage2.textContent = message1; }; 
+    });
+
+// Création d'un écouteur d'événement pour l'email
+email.addEventListener("keyup", function () { 
+    email.classList.remove("red", "green",);
+    errorMessage3.classList.add("d-none");  
+    if (regexEmail.test(email.value)) { 
+        email.classList.add("green"); 
+        errorMessage3.classList.add("d-none");
+    } else { 
+        email.classList.add("red"); 
+        errorMessage3.classList.remove("d-none");
+        errorMessage3.textContent = message2; };
+}); 
+
+// Création d'un écouteur d'événement pour le phone
+phone.addEventListener("keyup", function () { 
+    // Supprimer les classes "red" et "green" à chaque saisie 
+        phone.classList.remove("red", "green"); 
+        errorMessage4.classList.add("d-none");
+    //Test de la regexName pour tester la valeur
+        if (regexPhone.test(phone.value)) { 
+            phone.classList.add("green"); 
+            errorMessage4.classList.add("d-none");
+        } else { 
+            phone.classList.add("red"); 
+            errorMessage4.classList.remove("d-none");
+            errorMessage4.textContent = message1; }; 
+    });
+
+// Création d'un écouteur d'événement pour le mot de passe
+pwdMedium.addEventListener("input", function () { 
+    pwdMedium.classList.remove("red", "green",);
+    errorMessage4.classList.add("d-none");  
+    if (regexPwdStrong.test(pwdMedium.value)) { 
+        pwdMedium.classList.add("green"); 
+        errorMessage4.classList.add("d-none");
+    } else { 
+        pwdMedium.classList.add("red"); 
+        errorMessage4.classList.remove("d-none");
+        errorMessage4.textContent = message4; };
+}); 
+
+// Création d'un écouteur d'événement pour la confirmation du mot de passe
+pwdStrong.addEventListener("input", function () { 
+    pwdStrong.classList.remove("red", "green",);
+    errorMessage5.classList.add("d-none");  
+    if (regexPwdStrong.test(pwdStrong.value)) { 
+        pwdStrong.classList.add("green"); 
+        errorMessage5.classList.add("d-none");
+    } else { 
+        pwdStrong.classList.add("red"); 
+        errorMessage5.classList.remove("d-none");
+        errorMessage5.textContent = message4; };
+}); 
+
+// Fonction vérification des mots de passe
+pwdStrong.addEventListener("keyup", function () { 
+    // Supprimer les classes "red" et "green" à chaque saisie 
+        pwdMedium.classList.remove("red", "green");
+        pwdStrong.classList.remove("red", "green"); 
+        errorMessage9.classList.add("d-none");
+        errorMessage9.textContent = '';
+    // Conditions
+        if (pwdMedium.value == '' && pwdStrong.value == '') {
+            return
+        }
+        if (pwdMedium.value === pwdStrong.value) { 
+            pwdMedium.classList.add("green"); 
+            pwdStrong.classList.add("green");
+            errorMessage9.classList.add("d-none");
+        } else { 
+            pwdMedium.classList.add("red"); 
+            pwdStrong.classList.add("red");
+            errorMessage9.classList.remove("d-none");
+            errorMessage9.textContent = message3;
+        }
+    });
